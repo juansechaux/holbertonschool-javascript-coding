cb3aba4ebfa9d8a1528a536a0b75126c9bf85c4e
juansechaux <juansechaux@hotmail.com>
2024-01-24 18:03:14 -0500

    Task 4, script that prints the number of movies where the character Wedge Antilles is present

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/', (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const data = JSON.parse(response.body);
+    let count = 0;
+    for (const key of data.results) {
+      if (key.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count += 1;
+      }
+      // console.log(key['characters'])
+    }
+    console.log(count);
+    // console.log(data.results[1]['characters']);
+  }
+});
