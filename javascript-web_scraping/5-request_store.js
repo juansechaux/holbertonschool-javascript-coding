5a64664e3db796ac55aa0b203f1d2215ccca8777
juansechaux <juansechaux@hotmail.com>
2024-01-24 19:24:24 -0500

    Task 5, script that gets the contents of a webpage and stores it in a file, The first argument is the URL to request, The second argument the file path to store the body response.

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
+const fs = require('fs');
 
 const args = process.argv;
-request(args[2], )
+request(args[2], (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(args[3], response.body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
