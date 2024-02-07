c2ac54156428010223bed5782e632b2667532f93
juansechaux <juansechaux@hotmail.com>
2024-02-07 18:56:17 -0500

    Task 1, program named 1-stdin.js that will be executed through command line. display the message Welcome to Holberton School, what is your name? (followed by a new line). The user should be able to input their name on a new line. The program should display Your name is: INPUT. When the user ends the program, it should display This important software is now closing (followed by a new line).

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,12 +2,20 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
+function getName() {
+  const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
-    console.log('This important software is now closing')
+    console.log('This important software is now closing');
   }
-});
+}
 
-// console.log('This important software is now closing')
+process.stdin.on('readable', getName);
+
+// process.stdin.on('readable', function() {
+//   var chunk = process.stdin.read();
+//   if (chunk !== null) {
+//     process.stdout.write(`Your name is: ${chunk}`);
+//     console.log('This important software is now closing');
+//   }
+// });
