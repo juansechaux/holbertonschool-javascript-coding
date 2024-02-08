25ff3be7d883ccae0d22f66b57348f4e2a82245d
juansechaux <juansechaux@hotmail.com>
2024-02-08 16:24:16 -0500

    Task 2, function named countStudents. It accept a path in argument. If the database is not available, it should throw an error with the text Cannot load the database. If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS. It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES.

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    console.log('Number of students:', data.split('\n').length - 1);
+    const setFields = new Set();
+    const arrayLines = data.split('\n');
+    for (const i in arrayLines) {
+      if (Number(i) !== 0) {
+        const line = arrayLines[i].split(',');
+        setFields.add(line[3]);
+      }
+    }
+    for (const field of setFields) {
+      const studInField = [];
+      for (const line of arrayLines) {
+        const lineSplid = line.split(',');
+        if (field === lineSplid[3]) {
+          studInField.push(lineSplid[0]);
+        }
+      }
+      if (studInField.length !== 0) {
+        console.log(`Number of students in ${field}: ${studInField.length}. List: ${studInField}`);
+      }
+    }
+  });
+}
+
+module.exports = countStudents;
