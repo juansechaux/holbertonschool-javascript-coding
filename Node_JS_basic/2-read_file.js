2e99b9c08eef1a6b34bff717b0abf33ebcd08153
juansechaux <juansechaux@hotmail.com>
2024-02-08 17:37:27 -0500

    Task 2, function named countStudents. It accept a path in argument. If the database is not available, it should throw an error with the text Cannot load the database. If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS. It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES.

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -6,17 +6,18 @@ function countStudents(path) {
     const lines = data.trim().split('\n'); // trim() elimina las lineas en blanco al final
     console.log(`Number of students: ${lines.length - 1}`);
     const studentsByField = {};
-    for (let i = 1; i < lines.length; i++) {
+    for (let i = 1; i < lines.length; i += 1) {
       const [firstname, lastname, age, field] = lines[i].split(',');
 
       if (!studentsByField[field]) {
         studentsByField[field] = [];
       }
-
-      if (firstname && lastname && age && field) { // no tengo presente las lineas que no cumple con la estructura
+      // no tengo presente las lineas que no cumple con la estructura
+      if (firstname && lastname && age && field) {
         studentsByField[field].push(firstname);
       }
     }
+    // eslint-disable-next-line
     for (const field in studentsByField) {
       console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
     }
