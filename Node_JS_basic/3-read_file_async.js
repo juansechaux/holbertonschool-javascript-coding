bc6fc3755f61e46a24e7f98c226527fc20640781
juansechaux <juansechaux@hotmail.com>
2024-02-09 11:08:50 -0500

    Task 3, IMPORTANT: script should attempt to read the database file asynchronously. Function named countStudents. It accept a path in argument. If the database is not available, it should throw an error with the text Cannot load the database. If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS. It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES. Update the code to validate that the database have the correct structure.

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -6,26 +6,23 @@ function countStudents(path) {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        console.log('Number of students:', String(data.trim().split('\n').length - 1));
-        const setFields = new Set();
-        const arrayLines = data.trim().split('\n');
-        for (const i in arrayLines) {
-          if (Number(i) !== 0) {
-            const line = arrayLines[i].split(',');
-            setFields.add(line[3]);
+        const lines = data.trim().split('\n'); // trim() elimina las lineas en blanco al final
+        console.log(`Number of students: ${lines.length - 1}`);
+        const studentsByField = {};
+        for (let i = 1; i < lines.length; i += 1) {
+          const [firstname, lastname, age, field] = lines[i].split(',');
+
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          // no tengo presente las lineas que no cumple con la estructura
+          if (firstname && lastname && age && field) {
+            studentsByField[field].push(firstname);
           }
         }
-        for (const field of setFields) {
-          const studInField = [];
-          for (const line of arrayLines) {
-            const lineSplid = line.split(',');
-            if (field === lineSplid[3]) {
-              studInField.push(lineSplid[0]);
-            }
-          }
-          if (studInField.length !== 0) {
-            console.log(`Number of students in ${field}: ${studInField.length}. List: ${studInField.join(', ')}`);
-          }
+        // eslint-disable-next-line
+        for (const field in studentsByField) {
+          console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
         }
         resolve();
       }
