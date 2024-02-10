8613eb6e060211feb6f1fc67a2cd51563d9d137d
juansechaux <juansechaux@hotmail.com>
2024-02-10 10:41:14 -0500

    Task 5, test 2

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,52 +1,29 @@
+const url = require('url');
 const http = require('http');
 const fs = require('fs');
 
 const app = http.createServer((req, res) => {
-  const { url } = req;
+  const { pathname, searchParams } = url.parse(req.url, true);
 
-  if (url === '/') {
+  if (pathname === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
-  } else if (url === '/students') {
-    const dbPath = process.argv[2]; // Obtener la ruta del archivo de la base de datos del argumento de la línea de comandos
-    if (!dbPath) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Database file path not provided');
-      return;
+    res.end('Hello Holberton School!');
+  } else if (pathname === '/students') {
+    let dbName;
+    if (searchParams && searchParams.get('dbName')) {
+      dbName = searchParams.get('dbName');
+    } else {
+      dbName = 'database.csv';
     }
-    countStudents(dbPath)
-      .then(() => {
-        fs.readFile(dbPath, 'utf8', (err, data) => {
-          if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-          } else {
-            const studentsData = data.trim().split('\n');
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(studentsData.join('\n'));
-          }
-        });
-      })
-      .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(error.message);
-      });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found\n');
-  }
-});
 
-app.listen(1245);
-
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
+    fs.readFile(dbName, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        console.error(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error reading student data: Database file not found or inaccessible');
       } else {
         const lines = data.trim().split('\n');
-        console.log(`Number of students: ${lines.length - 1}`);
+        let response = `This is the list of our students:\nNumber of students: ${lines.length - 1}\n`;
         const studentsByField = {};
         for (let i = 1; i < lines.length; i += 1) {
           const [firstname, lastname, age, field] = lines[i].split(',');
@@ -57,11 +34,21 @@ function countStudents(path) {
             studentsByField[field].push(firstname);
           }
         }
+        // eslint-disable-next-line
         for (const field in studentsByField) {
-          console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+          response += `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}\n`;
         }
-        resolve();
+        response = response.slice(0, -1);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(response);
       }
     });
-  });
-}
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Unknown path!');
+  }
+});
+
+app.listen(1245, () => console.log('Server listening on port 1245'));
+
+module.exports = app;
